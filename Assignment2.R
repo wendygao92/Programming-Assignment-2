cbe1ce44ef57727d92718287614d0b9ad9453c6e
wenqiangao <Wendygao92@outlook.com>
2016-02-19 12:03:56 -0800

    Assignment2

---
+++ Assignment2.R
@@ -0,0 +1,21 @@
+makeCacheMatrix<-function(x=matrix()){
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function()x
+  setinv<-function(solve) inv<<- solve
+  getinv<-function()inv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)}
+casheSolve<-function(x,...){
+  inv<-x$getinv()
+  if(!is.null(inv)){
+    message("getting cashed data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinv(inv)
+  inv}
+
